a0c6ed13433f628d77df5b8c572f02c2eefddfe5
Sarah <sferguson6391@gmail.com>
2018-10-08 17:30:15 -0500

    calculator class added

---
+++ CG-14-1/CG-14-1/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_14_1
+{
+    class Calculator
+    {
+        public string Addition { get; set; }
+        public string Subtraction { get; set; }
+        public string Multiplication { get; set; }
+        public string Division { get; set; }
+
+        public void AdditionMethod(string addition, int number1, int number2, int total)
+        {
+            Addition = addition;
+            total = number1 + number2;
+        }
+
+        public void SubtractionMethod(string subtraction, int number1, int number2, int total)
+        {
+            Subtraction = subtraction;
+            total = number1 - number2;
+        }
+
+        public void MultiplicationMethod(string multiplication, int number1, int number2, int total)
+        {
+            Multiplication = multiplication;
+            total = number1 * number2;
+        }
+
+        public void DivisionMethod(string division, int number1, int number2, int total)
+        {
+            Division = division;
+            total = number1 / number2;
+        }
+    }
+}
