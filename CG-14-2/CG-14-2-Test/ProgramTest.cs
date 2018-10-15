4d99eb88822d7d546a8c6d3ea6dc20e052797aca
Sarah <sferguson6391@gmail.com>
2018-10-14 20:45:51 -0500

    updated 10/14/18

--- CG-14-2/CG-14-2-Test/ProgramTest.cs
+++ CG-14-2/CG-14-2-Test/ProgramTest.cs
@@ -6,18 +6,55 @@ namespace CG_14_2_Test
 {
     public class ProgramTest
     {
-        //addition
-        //subtraction
-        //multiplication
-        //division
-        //blank space
-        //no spaces
 
         [Fact]
         public void CalculatorMethod_addition()
         {
-            var result = ProgramTest.CalculatorMethod("2 + 2");
-            Assert.Equal("4", result);
+            var calculator = new Calculator();
+            var result = calculator.CalculatorMethod("2 + 2");
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void CalculatorMethod_subraction()
+        {
+            var calculator = new Calculator();
+            var result = calculator.CalculatorMethod("4 - 2");
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void CalculatorMethod_multiplication()
+        {
+            var calculator = new Calculator();
+            var result = calculator.CalculatorMethod("2 * 2");
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void CalculatorMethod_division()
+        {
+            var calculator = new Calculator();
+            var result = calculator.CalculatorMethod("2 / 2");
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void CalculatorMethod_blank()
+        {
+            var calculator = new Calculator();
+            try { var result = calculator.CalculatorMethod(""); }
+            catch (Exception)
+            { Assert.True(true); }
+        }
+
+        [Fact]
+        public void CalculatorMethod_nospaces()
+        {
+            var calculator = new Calculator();
+            try { var result = calculator.CalculatorMethod("2+2"); }
+            catch (Exception)
+            { Assert.True(true); }
         }
     }
 }
